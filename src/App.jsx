12e655dc65de5c6245051cf28917e7dd6cc55bf3
NozarLahooti <nozarlahooti@me.com>
2025-07-08 20:23:57 -0400

    Add main restaurant fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const[restaurants, setRestaurants] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const fetchRestaurants = async (query) => {
+    const options = {
+      method: 'GET',
+      url: 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants',
+      params: { locationId: '304554'},
+      headers: {
+        'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
+        'x-rapidapi-host': import.meta.env.VITE_RAPIDAPI_HOST,
+      },
+    }
+
+  try {
+    const response = await axios.request(options)
+    setRestaurants(response.data.data || [])
+  } catch (error) {
+    console.error('Error fetching restaurants:', error)
+  }
 }
 
-export default App
+return (
+  <div>
+    <h1> BiteFinder </h1>
+    <SearchBar onSearch={fetchRestaurants} />
+    <ul>
+      {restaurants.map((item, index) => (
+        <li key={index}>{item.name}</li>
+      ))}
+    </ul>
+  </div>
+)
+}
